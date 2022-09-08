01af6c4e2230acf749308187e295823f682668e1
Boldark <apaokagiashraf@gmail.com>
2022-09-08 16:01:12 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - print the string in the put function
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle"
- * Return: 0
- * */
+ * Return: 0 if success
+ */
 
 int main(void)
 
