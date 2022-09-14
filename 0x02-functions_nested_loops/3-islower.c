4b39c8a5234ae7ca55e959d44e3bbfa2e87951d4
Boldark <apaokagiashraf@gmail.com>
2022-09-14 14:08:56 -0700

    Fixed betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
  * _islower -> checks for lowercase characters
+ *
  * @c: character argument
  *
  * Return: Always 0 or 1 depending on the outcome
