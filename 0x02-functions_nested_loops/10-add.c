fce5055f8ed5772b2da4c45f7e6adfd4b0baf365
Boldark <apaokagiashraf@gmail.com>
2022-09-14 16:44:58 -0700

    Addition gang

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ *
+ * @a: first ninteger to be added
+ * @b: second integer to be added
+ *
+ * Return: Solution to the addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
