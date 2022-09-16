2647a99410ebdad9358dd0d8ab1a60aad4f4bca9
Boldark <apaokagiashraf@gmail.com>
2022-09-15 17:35:15 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints number
+ * @n: number to be printed
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		putchar('45');
+		x = -n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+		putchar(('x % 10) + '0');
+	}
+}
