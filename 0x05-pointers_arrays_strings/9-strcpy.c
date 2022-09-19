7abeee06f9584bf67a49626eadba77c3f10af4cd
Boldark <apaokagiashraf@gmail.com>
2022-09-19 06:56:11 -0700

    String copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies a string
+ * @dest: destination of the value
+ * @src: source of the value
+ * Return: destination of the value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[1] != '\0'; i ++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
