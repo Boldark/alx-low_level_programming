87b4245f8b13c5cd96fe8e809bd01c2e552576db
Boldark <apaokagiashraf@gmail.com>
2022-09-19 06:38:57 -0700

    String reversed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,4 +2,26 @@
 
 /**
  * rev_string - function that reverses a string
- *
+ * @s: string parameter
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, lenght1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[lentgh2] = tmp;
+		length2 -= 1;
+	}
+}
