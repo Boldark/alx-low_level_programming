1a27b97fcc7bce2e93682d95e9faab40eaa11df5
Boldark <apaokagiashraf@gmail.com>
2022-09-22 17:18:06 -0700

    Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,8 +13,15 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/* Below is my added line of code */
-(p + 5) = 98;
+/*
+ * Write your line of code here...
+ * You are not allowed to use a
+ * You are not allowed to modify p
+ * You can only make one statement
+ * You are not allowed to code anything else than this line of code
+ */
+*(p + 5) = 98;
+/* This makes it to prins 98\n */
 printf("a[2] = %d\n", a[2]);
 
 return (0);
