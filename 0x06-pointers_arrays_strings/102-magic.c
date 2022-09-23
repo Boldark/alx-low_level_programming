b53babf3f2372da0247ba8cafef0c3d7eeb9989b
Boldark <apaokagiashraf@gmail.com>
2022-09-22 17:11:27 -0700

    Magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+/* Below is my added line of code */
+(p + 5) = 98;
+printf("a[2] = %d\n", a[2]);
+
+return (0);
+}
