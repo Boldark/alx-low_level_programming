083c465aa969e9b4f18852a8975a3adc1cf2fa08
Boldark <apaokagiashraf@gmail.com>
2022-09-26 06:53:55 -0700

    Meme Copy fixed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
 /**
  * _memcpy - function that copies memory area.
  * @dest: destination to write to
- * @serc: source to copy from
+ * @src: source to copy from
  * @n: number of bytes to write
  * Return: pointer to dest
  */
