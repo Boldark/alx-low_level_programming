b94e029e3b7cf40873646b7053f831985be59a7a
Boldark <apaokagiashraf@gmail.com>
2022-10-04 05:14:32 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: parameter
+ * @argv: array of listed argument
+ * Return: Always 0 (Success0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
