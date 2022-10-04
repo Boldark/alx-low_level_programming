ff5da0b6d93fc24070e7e83d68933233b28a82e6
Boldark <apaokagiashraf@gmail.com>
2022-10-04 05:29:04 -0700

    My results

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: parameter
+ * @argv: array of listed command
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
