0627bdbf75d050e9f36196b70375f4429ab1594a
Boldark <apaokagiashraf@gmail.com>
2022-10-07 14:07:44 -0700

    3 tasks completed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - function to create an array
+ * @size: size of array
+ * @c: initialized character
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
