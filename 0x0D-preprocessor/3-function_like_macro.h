68204519bbc4a55dbe1d362f8ace262caaf1a86b
Boldark <apaokagiashraf@gmail.com>
2022-10-20 15:22:48 -0700

    Finished

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#ifndef ABSO
+#define ABSO
 
-#endif
+#define ABS(X) (((X) < (0)) ? (-(X)) : (X))
+
+#endif /* ABS(X) */
