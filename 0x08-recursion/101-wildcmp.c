1a4d9bafc83fa6dd6d48fbc12ed9a732f3f1d661
Boldark <apaokagiashraf@gmail.com>
2022-10-20 15:00:19 -0700

    Finished

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-int bandersnatch(char *s1, char *s2);
-
-char *move(char *s2);
 /**
  * wildcmp - function that compares 2 strings to check if they are identical
  * @s1: string 1
@@ -12,51 +9,13 @@ char *move(char *s2);
 
 int wildcmp(char *s1, char *s2)
 {
-	int sum = 0;
-
-	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
 	if (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
-
-	if (*s1 == '\0' || *s2 == '\0')
-		return (0);
-
 	if (*s2 == '*')
-	{
-		s2 = move(s2);
-
-	if (*s2 == '\0')
-		return (1);
-
-	if (*s1 == *s2)
-		sum += wildcmp(s1 + 1, s2 + 1);
-	sum += bandersnatch(s1 + 1, s2);
-	return (!!sum);
-	}
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
-}
-
-int bandersnatch(char *s1, char *s2)
-{
-	if (*s1 == '\0')
-		return (0);
-
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
-	return (bandersnatch(s1 + 1, s2));
-}
-
-char *move(char *s2)
-{
-	if (*s2 == 'a*')
-		return (move(s2 + 1));
-
-	else
-		return (s2);
 }
