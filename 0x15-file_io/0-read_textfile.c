995c8fe2ab7d04f615dab5f6bae248362d5d10d8
Boldark <apaokagiashraf@gmail.com>
2022-11-07 12:25:34 -0800

    Possible fix for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - function that Reads a text file and prints it to POSIX stdout.
@@ -8,30 +12,34 @@
  * Return: 0 if filename fails or is NULL
  * O/w - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t op, rd, wr;
+	int fd;
+	ssize_t lenr, lenw;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		close(fd);
 		return (0);
-
-	op = open(filename, O_RDONLY);
-	rd = read(op, buffer, letters);
-	wr = write(STDOUT_FILENO, buffer, rd);
-
-	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	close(op);
-
-	return (wr);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
